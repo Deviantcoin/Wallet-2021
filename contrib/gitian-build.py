2842f2d59a5f5a2a26736cf4cb3732ed983f9d21
Liquid <Liquid369@gmail.com>
2021-11-28 14:20:01 -0600

    Adjust directory

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -110,7 +110,7 @@ def setup_repos():
     if not os.path.isdir('gitian-builder'):
         subprocess.check_call(['git', 'clone', 'https://github.com/devrandom/gitian-builder.git'])
     if not os.path.isdir('Deviant-Core'):
-        subprocess.check_call(['git', 'clone', 'https://github.com/deviantnode/Deviant-Core.git'])
+        subprocess.check_call(['git', 'clone', 'https://github.com/Deviantcoin/Wallet-4.1.x.git'])
     os.chdir('gitian-builder')
     make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
     if args.docker:
